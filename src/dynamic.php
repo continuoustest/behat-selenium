3a5e9c7961aa91ad163b1d8ae25fa9e4bcc936ea
Pierre Tomasina <tomasinap@gmail.com>
2017-02-28 12:07:43 +0100

    debug

--- src/dynamic.php
+++ src/dynamic.php
@@ -6,6 +6,9 @@ echo "\n\n --------------- \n\n";
 
 echo "clear_env: ", ini_get('clear_env'), "\n";
 
+echo "BEHAT_BASE_URL: ", getenv('BEHAT_BASE_URL'), "\n";
+echo "BEHAT_WD_HOST: ", getenv('BEHAT_WD_HOST'), "\n";
+
 echo "phpversion: ", phpversion(), "\n";
 echo "environments: ", print_r($_ENV, true), "\n";
 echo "server: ", print_r($_SERVER, true), "\n";
